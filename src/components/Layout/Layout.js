11888dca9eb6c7e125cf6925cd6609ba9ecab2c3
David Smejkal <xsmejk10@node.mendelu.cz>
2019-10-18 15:17:55 +0200

    set files

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,1 @@
-import React, {Fragment} from 'react';
-import styles from './Layout.css';
-
-const Layout = ({children}) => (
-    <Fragment>
-        <div>rest of layout</div>
-        <div>
-            {children}
-        </div>
-    </Fragment>
-);
-
-export default Layout;
+import React, {Fragment} from 'react';import styles from './Layout.css';const Layout = ({children}) => (    <Fragment>        <div>rest of layout</div>        <div>            {children}        </div>    </Fragment>);export default Layout;
